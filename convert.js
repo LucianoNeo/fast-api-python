caa7dfb8461abf72cd4a944fa44a9c7104d264d0
LUCIANO DOS SANTOS BUENO <tec.info.luciano@hotmail.com>
2022-12-21 21:32:11 -0300

    js fix 2

--- convert.js
+++
@@ -1,9 +0,0 @@
-const ffmpeg = require("fluent-ffmpeg");
-
-ffmpeg()
-  .input("testes/audio.mp3")
-  .output("output.wav")
-  .on("end", () => {
-    console.log("Conversion completed!");
-  })
-  .run();
