ee4989ed26b002601450d8280bba216e1a883838
LUCIANO DOS SANTOS BUENO <tec.info.luciano@hotmail.com>
2022-12-21 21:30:08 -0300

    js fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import speech_recognition as sr
 from fastapi import FastAPI, File, Form, HTTPException, UploadFile
-from pydub import AudioSegment
 
 app = FastAPI()
 
@@ -13,17 +12,14 @@ def home():
 async def create_upload_audio(file: UploadFile):
     filename = file.filename
     extension = file.filename.split(".")[1]
-    if extension == 'wav' or extension == 'mp3':
+    if extension == 'wav':
         with open(filename, "wb") as f:
             contents = await file.read()
             f.write(contents)
             r = sr.Recognizer()
-            audio = AudioSegment.from_file(filename, format="mp3")
-            audio.export("output.wav", format="wav")
-            with sr.AudioFile("output.wav") as source:
+            with sr.AudioFile(filename) as source:
                 audioStored = r.record(source)
-                
                 text = r.recognize_google(audioStored, language='pt-BR')
                 return text
     else:
-        raise HTTPException(status_code=500, detail='Formato não aceito no momento, tente com .wav ou .mp3')
+        raise HTTPException(status_code=500, detail='Formato não aceito no momento, tente com .wav')
